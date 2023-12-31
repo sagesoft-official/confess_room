8e34daa1b503554dff37f64f7d9697be7750a21e
SHDocter <shdocter@nya-wsl.com>
2024-01-01 05:28:15 +0800

    v1.0.0

--- README.md
+++ README.md
@@ -1,2 +1,47 @@
-# confess
- 桥洞教堂忏悔室（基于NiceGUI的提问箱）
+<!--
+ * @Author: Nya-WSL
+ * Copyright © 2024 by Nya-WSL All Rights Reserved. 
+ * @Date: 2024-01-01 02:52:15
+ * @LastEditors: 狐日泽
+ * @LastEditTime: 2024-01-01 05:27:44
+-->
+# Confess Room
+
+桥洞教堂忏悔室（基于NiceGUI的提问箱/棉花糖）
+
+## USAGE
+
+### SERVER
+
+```
+# Port
+
+python main.py
+
+http://127.0.0.1:11452
+
+# Nginx
+
+将nginx_conf所有的domain和第15行的端口替换成你自己的并复制到"/etc/nginx/site-enabled/default"末尾然后重启nginx（systemctl restart nginx）
+
+如需https访问需自行申请证书或通过certbot配置
+
+```
+
+### LOCAL
+
+本程序是基于服务器多用户访问设计的，无法在本地正常运行
+
+本地穿透请参考上方，Windows需自行解决反代配置，宝塔等面板服务器需自行解决nginx配置
+
+## KNOWN ISSUES
+
+- GUI是基于 `24` 寸显示器 + `1920*1080` 分辨率 + `100%` 缩放设计的，在 `1080p+` 和 `100%+` 缩放的设备上可能存在适配问题，移动设备除了平板和折叠屏应该没有这个问题
+
+- 适配问题在 `100%+` 缩放的设备上可能更容易出现
+
+- 如有问题可尝试删除 `ui.textarea` 和 `ui.button` 以外的组件或自行适配
+
+- 移动设备无法拖动文本区，这是组件限制，我们无法解决
+
+- 暂无配套的读取棉花糖
--- main.py
+++ main.py
@@ -3,59 +3,63 @@ Author: Nya-WSL
 Copyright © 2023 by Nya-WSL All Rights Reserved. 
 Date: 2023-12-31 16:43:50
 LastEditors: 狐日泽
-LastEditTime: 2023-12-31 21:44:18
+LastEditTime: 2024-01-01 05:26:12
 '''
 
 import json
-from nicegui import ui
 from uuid import uuid4
 from router import Router
+from nicegui import ui, app
 from datetime import datetime
 from typing import List, Tuple
 
 # messages: List[Tuple[str, str, str, str]] = []
+app.add_static_files('/static', 'static')
 
-@ui.page('/')  # normal index page (e.g. the entry point of the app)
-@ui.page('/{_:path}')  # all other pages will be handled by the router but must be registered to also show the SPA index page
+@ui.page('/')
+# @ui.page('/{_:path}')
 def main():
     router = Router()
     page_id = str(uuid4())
 
     @router.add('/')
     def init():
-        ui.badge('赎罪券投放处', outline=True).classes('text-2xl')
+        ui.badge('桥洞教堂赎罪券投放处', outline=True).classes('text-2xl absolute top-1/3 left-1/2 translate-x-[-50%]')
 
     @router.add(f'/{page_id}')
     def index():
         button.set_visibility(False)
-        async def send():
+
+        def send():
             if text.value == '':
-                ui.notify('虚假的赎罪是会被修女诅咒的！', type="positive")
+                ui.notify('虚假的赎罪是会被修女诅咒的！', type="negative", position="top")
                 return
             with open('message.json', 'r', encoding="utf-8") as f:
                 message_json = json.load(f)
             message_json[f"{datetime.now().strftime('%Y-%m-%d-%H-%M-%S')}"] = text.value
             with open('message.json', 'w+', encoding="utf-8") as f:
                 json.dump(message_json, f, indent=4, ensure_ascii=False)
-            text.set_value('')
-            ui.notify('修女会保佑你的...')
+            ui.notify('修女会保佑你的...', type="positive", position="top")
 
-        # user_id = str(uuid4())
-        # avatar = f'https://robohash.org/{user_id}?bgset=bg2'
+        def call():
+            send()
+            text.set_value('')
 
         with ui.row().classes('w-full no-wrap items-center'):
-            # with ui.avatar().on('click', lambda: ui.open(main)):
-            #     ui.image(avatar)
-            text = ui.textarea(placeholder='按回车投放赎罪券').on('keydown.enter', send).props('rounded outlined input-class=mx-3').classes('flex-grow')
-        ui.markdown('桑尾草原赎罪券投放处').classes('text-xs self-end mr-8 text-primary')
+            text = ui.textarea(placeholder='忏悔内容').props('rounded outlined input-class=mx-3').classes('flex-grow text-white')
+            ui.button('忏悔', on_click=lambda: call()).classes("absolute top-1/2 left-1/2 translate-x-[-50%]")
+        ui.markdown('桑尾草原赎罪券投放处').classes('text-xs self-end mr-8 text-pink p-2')
 
-        # with ui.column().classes('w-full max-w-2xl mx-auto items-stretch'):
-        #     chat_messages(user_id)
-
+    ui.query('body').style('background: url("static/bg.jpg") 0px 0px/cover')
     with ui.row():
-        button = ui.button('初始化', on_click=lambda: router.open(index)).classes('w-32')
+        button = ui.button('向桥洞修女发起忏悔', on_click=lambda: router.open(index), color="pink").classes("absolute top-1/2 left-1/2 translate-x-[-50%]")
+        ui.badge("注意事项", color="lightpink", text_color="white").classes("text-xl absolute top-2/3 left-1/2 translate-x-[-50%]")
+        ui.badge("忏悔页面阅后即焚", color="lightpink", text_color="white").classes("text-xl absolute top-2/3 left-1/2 translate-x-[-50%] translate-y-[110%]")
+        ui.badge("刷新或关闭后页面将立即销毁", color="lightpink", text_color="white").classes("text-xl absolute top-2/3 left-1/2 translate-x-[-50%] translate-y-[220%]")
+        ui.badge("如需多次忏悔，可在忏悔后继续忏悔", color="lightpink", text_color="white").classes("text-xl absolute top-2/3 left-1/2 translate-x-[-50%] translate-y-[330%]")
+        ui.badge("请勿忏悔违法、违规、禁播内容", color="lightpink", text_color="white").classes("text-xl absolute top-2/3 left-1/2 translate-x-[-50%] translate-y-[440%]")
 
-    # this places the content which should be displayed
-    router.frame().classes('w-full p-4 bg-gray-100')
+    # 不可删除
+    router.frame().classes('w-full')
 
-ui.run(title="桥洞教堂忏悔室", favicon="./icon.ico", host="0.0.0.0", port=11452, language="zh-CN", show=False)
+ui.run(title="桥洞教堂忏悔室", favicon="static/icon.ico", host="0.0.0.0", port=11452, language="zh-CN", show=False)
