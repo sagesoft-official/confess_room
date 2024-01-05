3be411279491bca42557430e608ca58bb2523a7d
SHDocter <shdocter@nya-wsl.com>
2024-01-06 06:24:11 +0800

    v1.2.0
    - 主页标题新增版本号
    
    - 修改所有按钮的样式
    
    - 部分组件颜色改为#E6354F
    
    - 投稿页面新增返回主页按钮
    
    - 现在投稿可以输入自定义昵称，留空默认为投稿时间
    
    - 棉花糖页面新增可切换的紧凑模式，默认为关，该模式可能会缓解当某一条棉花糖有过多换行符时会导致背景Y轴被严重拉伸的问题
    
    - 尝试通过将注意事项部分组件的值改为HTML元素来缓解部分设备的排版问题
    
    - 修复因为显示棉花糖的组件的值是HTML元素导致的棉花糖中的换行符无效的问题
    
    - 修复一个在服务器反代环境下头像文件路径错误的问题

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ Author: Nya-WSL
 Copyright © 2023 by Nya-WSL All Rights Reserved. 
 Date: 2023-12-31 16:43:50
 LastEditors: 狐日泽
-LastEditTime: 2024-01-05 22:47:14
+LastEditTime: 2024-01-06 06:19:25
 '''
 
 import os
@@ -20,7 +20,7 @@ from fastapi.responses import RedirectResponse
 # from starlette.middleware.base import BaseHTTPMiddleware
 
 # messages: List[Tuple[str, str, str, str]] = []
-version = "1.1.3"
+version = "1.2.0"
 app.add_static_files('/static', 'static')
 passwords = {'Sage': 'b10b88afa32c8c74941f600bb4507e6cbd5fb336bc82390ab0bbe9da07f08e90', 'sage': 'b10b88afa32c8c74941f600bb4507e6cbd5fb336bc82390ab0bbe9da07f08e90', 'sagesoft': '6a2c966fa4655342b1e8e2e2978a666bbb5971722c2f173ac13e848a0728f68f'}
 
@@ -38,7 +38,7 @@ def login() -> Optional[RedirectResponse]:
         return RedirectResponse('/messages')
     else:
         with ui.card().classes('absolute-center'):
-            ui.badge('桥洞教堂忏悔室登记处', outline=True, color='pink').classes('text-xl')
+            ui.badge('桥洞教堂忏悔室登记处', outline=True, color='', text_color='#E6354F').classes('text-xl')
             username = ui.input('来访人').on('keydown.enter', try_login)
             password = ui.input('密码', password=True, password_toggle_button=True).on('keydown.enter', try_login)
             with ui.row():
@@ -52,8 +52,9 @@ def page():
         return RedirectResponse('/login')
     else:
         with ui.row():
-            ui.button('Quit', on_click=lambda: (app.storage.user.clear(), ui.open('/')))
-            ui.button('Back', on_click=lambda: ui.open('/'))
+            ui.button('Quit', on_click=lambda: (app.storage.user.clear(), ui.open('/'))).classes('bg-transparent')
+            ui.button('Back', on_click=lambda: ui.open('/')).classes('bg-transparent')
+            show_module = ui.switch(text="紧凑模式", value=False, on_change=lambda: ui.open("/messages")).bind_value(app.storage.user, "show_module")
         if not os.path.exists('message.json'):
             with open('message.json', 'w', encoding="utf-8") as f:
                 f.write(r'{}')
@@ -63,14 +64,15 @@ def page():
                 message_json = json.load(f)
         
         if message_json == {}:
-            with ui.card().classes('absolute-center pink'):
-                ui.badge('目前没人前来忏悔...', outline=True, color="pink").classes('text-xl')
+            with ui.card().classes('absolute-center bg-transparent'):
+                ui.badge('目前没人前来忏悔...', outline=True, color="", text_color='#E6354F').classes('text-xl')
         else:
             for key,value in dict(message_json).items():
                 with ui.expansion(key).classes('w-full'):
-                    # ui.textarea(value=value).classes('text-xl w-full')
-                    ui.chat_message(f'<font size="5">{value}</font>',
-            avatar='/static/bg.jpg', text_html=True).props('bg-color="green-1"')
+                    if show_module.value:
+                        ui.textarea(value=value).classes('text-xl w-full').props('outlined readonly bg-color="green-1"')
+                    else:
+                        ui.chat_message(value, avatar='static/bg.jpg').props('bg-color="green-1"').classes('text-h6')
                 ui.separator()
 
 @ui.page('/')
@@ -81,11 +83,11 @@ def main():
 
     @router.add('/')
     def init():
-        ui.badge('桥洞教堂赎罪券投放处', outline=True).classes('text-2xl absolute top-1/3 left-1/2 translate-x-[-50%]')
+        ui.badge(f'桥洞教堂赎罪券投放处v{version}', outline=True, text_color="#E6354F", color="").classes('text-2xl absolute top-1/3 left-1/2 translate-x-[-50%]')
 
     @router.add('/nicegui/')
     def init():
-        ui.badge('桥洞教堂赎罪券投放处', outline=True).classes('text-2xl absolute top-1/3 left-1/2 translate-x-[-50%]')
+        ui.badge(f'桥洞教堂赎罪券投放处v{version}', outline=True, text_color="#E6354F", color="").classes('text-2xl absolute top-1/3 left-1/2 translate-x-[-50%]')
 
     @router.add(f'/{page_id}')
     def index():
@@ -103,7 +105,10 @@ def main():
             else:
                 with open('message.json', 'r', encoding="utf-8") as f:
                     message_json = json.load(f)
-            message_json[f"{datetime.now().strftime('%Y-%m-%d %H:%M:%S')}"] = text.value
+            if author.value == "":
+                message_json[f"{datetime.now().strftime('%Y-%m-%d %H:%M:%S')}"] = text.value
+            else:
+                message_json[f"{author.value} | {datetime.now().strftime('%Y-%m-%d %H:%M:%S')}"] = text.value
             with open('message.json', 'w+', encoding="utf-8") as f:
                 json.dump(message_json, f, indent=4, ensure_ascii=False)
             ui.notify('修女会保佑你的...', type="positive", position="top")
@@ -112,20 +117,33 @@ def main():
             send()
             text.set_value('')
 
-        with ui.row().classes('w-full no-wrap items-center'):
-            text = ui.textarea(placeholder='忏悔内容').props('rounded outlined input-class=mx-3').classes('flex-grow text-white')
-            ui.button('忏悔', on_click=lambda: call()).classes("absolute top-1/2 left-1/2 translate-x-[-50%]")
-        ui.markdown('桑尾草原赎罪券投放处').classes('text-xs self-end mr-8 text-pink p-2')
+        def back():
+            with ui.dialog() as dialog, ui.card():
+                ui.label('忏悔内容将不会被保存，确定要返回吗？')
+                with ui.row().classes('w-full'):
+                    ui.button('确定', on_click=lambda: ui.open('/'), color='#E6354F').classes("text-white")
+                    ui.button('取消', on_click=dialog.close, color='#E6354F').classes("text-white")
+            dialog.open()
+
+        with ui.row().classes('w-full no-wrap'):
+            author = ui.input(label="称呼(非必填)").props('input-class=mx-3').classes("absolute-center translate-x-[-50%] translate-y-[-200%]")
+            text = ui.textarea(placeholder='忏悔内容').props('rounded outlined input-class=mx-3"').classes('flex-grow')
+            # ui.button('忏悔', on_click=lambda: call(), color='#E6354F').classes("absolute top-1/2 left-1/2 translate-x-[-50%] text-white")
+            with ui.row().classes("absolute-center text-white"):
+                ui.button('忏悔', on_click=lambda: call(), color='#E6354F').classes("text-white")
+                ui.button('返回', on_click=lambda: back(), color='#E6354F').classes("text-white")
+        ui.markdown('桑尾草原赎罪券投放处').classes('text-xs self-end mr-8 p-2').style('color: rgb(230 53 79)')
 
     ui.query('body').style('background: url("static/bg.jpg") 0px 0px/cover')
     with ui.row():
-        send_button = ui.button('向桥洞修女发起忏悔', on_click=lambda: router.open(index), color="pink").classes("absolute top-1/2 left-1/2 translate-x-[-50%]")
-        login_button = ui.button('桥洞教堂忏悔录', on_click=lambda: ui.open('/login'), color="pink").classes("absolute top-1/2 left-1/2 translate-x-[-50%] translate-y-[200%]")
-        ui.badge("注意事项", color="lightpink", text_color="white").classes("text-xl absolute top-2/3 left-1/2 translate-x-[-50%]")
-        ui.badge("忏悔页面阅后即焚", color="lightpink", text_color="white").classes("text-xl absolute top-2/3 left-1/2 translate-x-[-50%] translate-y-[110%]")
-        ui.badge("刷新或关闭后页面将立即销毁", color="lightpink", text_color="white").classes("text-xl absolute top-2/3 left-1/2 translate-x-[-50%] translate-y-[220%]")
-        ui.badge("如需多次忏悔，可在忏悔后继续忏悔", color="lightpink", text_color="white").classes("text-xl absolute top-2/3 left-1/2 translate-x-[-50%] translate-y-[330%]")
-        ui.badge("请勿忏悔违法、违规、禁播内容", color="lightpink", text_color="white").classes("text-xl absolute top-2/3 left-1/2 translate-x-[-50%] translate-y-[440%]")
+        send_button = ui.button('向桥洞修女发起忏悔', on_click=lambda: router.open(index), color="#E6354F").classes("absolute top-1/2 left-1/2 translate-x-[-50%] text-white")
+        login_button = ui.button('桥洞教堂忏悔录', on_click=lambda: ui.open('/login'), color="#E6354F").classes("absolute top-1/2 left-1/2 translate-x-[-50%] translate-y-[200%] text-white bg-transparent")
+        with ui.badge(outline=True, color="", text_color="#E6354F").classes("absolute top-2/3 left-1/2 translate-x-[-50%]"):
+            ui.html('<center>注意事项<br>忏悔页面阅后即焚<br>刷新或关闭后页面将立即销毁<br>如需多次忏悔，可在忏悔后继续忏悔<br>请勿忏悔违法、违规、禁播内容</center>').classes('text-xl')
+            # ui.badge("忏悔页面阅后即焚", color="lightpink", text_color="white").classes("text-xl absolute top-2/3 left-1/2 translate-x-[-50%] translate-y-[110%]")
+            # ui.badge("刷新或关闭后页面将立即销毁", color="lightpink", text_color="white").classes("text-xl absolute top-2/3 left-1/2 translate-x-[-50%] translate-y-[220%]")
+            # ui.badge("如需多次忏悔，可在忏悔后继续忏悔", color="lightpink", text_color="white").classes("text-xl absolute top-2/3 left-1/2 translate-x-[-50%] translate-y-[330%]")
+            # ui.badge("请勿忏悔违法、违规、禁播内容", color="lightpink", text_color="white").classes("text-xl absolute top-2/3 left-1/2 translate-x-[-50%] translate-y-[440%]")
 
     # 不可删除
     router.frame().classes('w-full')
